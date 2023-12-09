cd206bcf154caec1aab476f826ccfcb19f5b388b
Agnes <agnes.mccall5@gmail.com>
2023-12-08 20:41:53 -0500

    calculate monthly payment

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -35,6 +35,10 @@ function update() {
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let i = values.rate / 12;
+  let n = values.years * 12;
+  let monthlyPayment = (values.amount * i) / (1 - (1 + i) ** -n);
+  return monthlyPayment.toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
