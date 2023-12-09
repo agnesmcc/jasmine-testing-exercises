58026c25639167fbcbd5c8154ccb6c6cb9d19e8b
Agnes <agnes.mccall5@gmail.com>
2023-12-08 21:43:59 -0500

    Add additional tests to servers.test.js

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,6 +1,9 @@
 describe("Servers test (with setup and tear-down)", function() {
   beforeEach(function () {
     // initialization logic
+    allServers = {};
+    let serverTbody = document.querySelector('#serverTable tbody');
+    serverTbody.innerHTML = "";
     serverNameInput.value = 'Alice';
   });
 
@@ -11,8 +14,25 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should add all servers on updateServerTable()', function(){
+    allServers = {
+      "server0": { "serverName": "Alice"},
+      "server1": { "serverName": "Tom"},
+      "server2": { "serverName": "Jim"}
+    }
+    expect(Object.keys(allServers).length).toEqual(3);
+    expect(allServers['server0'].serverName).toEqual('Alice');
+    updateServerTable();
+    const table = document.querySelector('#serverTable');
+    const thirdRow = table.querySelector('tr:nth-child(3)');
+    const firstTdValue = thirdRow.querySelector('td').innerText;
+    expect(firstTdValue).toEqual("Jim");
+  });
+
   afterEach(function() {
     let serverTbody = document.querySelector('#serverTable tbody');
     serverTbody.innerHTML = "";
+    serverNameInput.value = '';
+    allServers = {};
   });
 });
