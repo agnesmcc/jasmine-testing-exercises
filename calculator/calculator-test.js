b81ca730da15527b671fcda9de6721299ed49251
Agnes <agnes.mccall5@gmail.com>
2023-12-08 21:11:01 -0500

    Add loan calc tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,19 @@
 
+let values = {
+  amount: 1000,
+  years: 2,
+  rate: 2.99
+}
+
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  let monthly = calculateMonthlyPayment(values);
+  expect(monthly).toEqual("250.37");
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  let monthly = calculateMonthlyPayment(values);
+  expect(parseFloat(monthly)).toBeCloseTo(250.37, 2);
 });
 
 /// etc
